1b45d6c8899c031931e4ce5d515760652c1f578b
bakytdeveloper <bakytdeveloper@gmail.com>
2026-04-16 17:05:27 +0600

    Поправил индикатор стрелку на главном баннере, в компоненте Hero

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -46,13 +46,13 @@ const Hero = () => {
                             <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                     </Link>
-                    <button className="btn btn-outline hero-video-btn">
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3 9.5L3 14.5C3 18 5 20 8.5 20H15.5C19 20 21 18 21 14.5V9.5C21 6 19 4 15.5 4H8.5C5 4 3 6 3 9.5Z" stroke="currentColor" strokeWidth="1.5"/>
-                            <path d="M9.5 8.5L9.5 15.5C9.5 16.7 10.8 17.4 11.8 16.7L16.5 13.7C17.4 13.1 17.4 11.9 16.5 11.3L11.8 8.3C10.8 7.6 9.5 8.3 9.5 9.5V8.5Z" stroke="currentColor" strokeWidth="1.5"/>
-                        </svg>
-                        Видео о компании
-                    </button>
+                    {/*<button className="btn btn-outline hero-video-btn">*/}
+                    {/*    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+                    {/*        <path d="M3 9.5L3 14.5C3 18 5 20 8.5 20H15.5C19 20 21 18 21 14.5V9.5C21 6 19 4 15.5 4H8.5C5 4 3 6 3 9.5Z" stroke="currentColor" strokeWidth="1.5"/>*/}
+                    {/*        <path d="M9.5 8.5L9.5 15.5C9.5 16.7 10.8 17.4 11.8 16.7L16.5 13.7C17.4 13.1 17.4 11.9 16.5 11.3L11.8 8.3C10.8 7.6 9.5 8.3 9.5 9.5V8.5Z" stroke="currentColor" strokeWidth="1.5"/>*/}
+                    {/*    </svg>*/}
+                    {/*    Видео о компании*/}
+                    {/*</button>*/}
                 </div>
 
                 <div className="hero-stats" data-aos="fade-up" data-aos-delay="400">
